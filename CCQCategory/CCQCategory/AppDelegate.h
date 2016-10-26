69d06807a4704fcdec05e3d9e58e4c2633a40708
sundusk <zhuyue346@gmail.com>
2016-10-26 15:02:55 +0800

    创建项目

---
+++ CCQCategory/CCQCategory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CCQCategory
+//
+//  Created by 夜兔神威 on 16/10/26.
+//  Copyright © 2016年 ccq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CCQCategory/CCQCategory/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CCQCategory
+//
+//  Created by 夜兔神威 on 16/10/26.
+//  Copyright © 2016年 ccq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
